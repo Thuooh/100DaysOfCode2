0bc840ebebce65a53302a92648fa088adc3e069b
Thuooh <89125991+Thuooh@users.noreply.github.com>
2025-02-26 22:52:48 +0300

    Add files via upload

---
+++ DayOne.py
@@ -0,0 +1,24 @@
+# A refresher on python basic functions
+
+# IF/Else
+if (2 > 1) == (5 < 10):
+  print('Both expressions give the same result')
+
+c = '2'
+d = 2
+
+if c == d:
+  print('They are equal')
+else:
+  print('They are not equal')
+
+# Not equal 
+
+val1 = 10
+val2 = 20
+
+if val1 != val2:
+  print("They are NOT equal")
+
+if (10 > 1) != (10 > 1000):
+  print("They are NOT equal")
